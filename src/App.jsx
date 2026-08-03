da2d2b07031c683c11c82443ffa5e3d47f30af81
namidigital <njohansenjr@gmail.com>
2026-08-03 18:34:07 -0400

    Implement addTodo with useState, handle form submission with useRef

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import './App.css';
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
-const todos = [
-  { id: 1, title: 'review resources' },
-  { id: 2, title: 'take notes' },
-  { id: 3, title: 'code out app' },
-];
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle) {
+    const newTodo = { id: Date.now(), title: todoTitle };
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );
